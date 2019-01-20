ee59fbfe40c343064e32e0d4553539666e2ac53e
Мичил <user@macbook-user.local>
2019-01-20 13:26:43 +0300

    Added Unit Test Changed test scheme in .travis.yml

--- IndigearTests/IndigearTests.swift
+++ IndigearTests/IndigearTests.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Michil. All rights reserved.
 //
 
+import Foundation
 import XCTest
 @testable import Indigear
 
@@ -19,16 +20,42 @@ class IndigearTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func testIndigear(){
+        
+        let e = expectation(description: "Indigear")
+        
+        let params = ["param1" : "value1"]
+        
+        let requestURL = "http://indigear.michil.ru"
+        
+        Indigear.run(requestURL, method: .post, body: params) { result in
+            if result.isSuccess {
+                do {
+                    let result = try JSONSerialization.jsonObject(with: result.result!, options: []) as! [String : Any]
+                    
+                    XCTAssertNotNil(result, "No response")
+                    
+                    var postRequests = [String : String]()
+                    postRequests = result["POST"] as! [String : String]
+                    
+                    if (postRequests == params) {
+                        e.fulfill()
+                    }else{
+                        XCTFail("Requests not equals to responce")
+                    }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+                } catch {
+                    XCTAssertNil(error, "Error: \(error.localizedDescription)")
+                }
+                
+            }else{
+                XCTAssertNil(result.error, "Request error: \(result.error!.localizedDescription)")
+            }
         }
+        
+        waitForExpectations(timeout: 10.0, handler: nil)
+
+        
     }
 
 }
